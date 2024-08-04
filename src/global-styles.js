1e170419b032be00dc79ddd8888875c00d9791e2
benf1 <bdfarhi@buffalo.edu>
2024-08-04 17:03:44 -0400

    jumbotron component

---
+++ src/global-styles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+html, body {
+    font-family: 'Helvetica Heue', Helvetica, Arial, sans-serif;
+    -webkit-font-soothing:antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: black ;
+    color: #333333;
+    font-size:16px;
+}`;
